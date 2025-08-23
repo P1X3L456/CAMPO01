c8a770bb8c3ba3e87620efa99514ae55b69ff711
Silvio Porturas <silvioporturas173@gmail.com>
2025-08-23 00:00:08 -0500

    Proyecto Campo01

---
+++ SEMANA01/src/main/java/com/mycompany/semana01/SEMANA01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.semana01;
+
+/**
+ *
+ * @author USUARIO1
+ */
+public class SEMANA01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
